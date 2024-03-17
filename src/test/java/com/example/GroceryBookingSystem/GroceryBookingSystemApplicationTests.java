c5006f489ae29608aa95418c4a632c4cb7b6c15a
akash <akash.bande.work@gmail.com>
2024-03-17 12:33:47 +0530

    Initial commit

---
+++ src/test/java/com/example/GroceryBookingSystem/GroceryBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GroceryBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
